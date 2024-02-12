ab982af85df46f20d8348ff3cb74a7e9b96b8353
Max <maxmais10@gmail.com>
2024-02-12 13:33:23 +1000

    refactor brain_games.py, cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,7 @@
 import prompt
 
-name = prompt.string('May I have your name?')
-
 
 def welcome_user():
-    print(f'Hello,  {name.capitalize()}!')
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your Name?\n')
+    print(f'Hello, {name.capitalize()}!')
