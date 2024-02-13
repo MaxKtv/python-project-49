afc2e7aa025a124f320e3e5c872549b4ec4caa61
Max <maxmais10@gmail.com>
2024-02-13 15:15:48 +1000

    add new brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,47 @@
+import random
+import prompt
+
+print('Welcome to the Brain Games!')
+name = prompt.string('May I have your Name?\n')
+print(f'Hello, {name.capitalize()}!')
+print('What number is missing in the progression?')
+
+
+
+def random_generator():
+    return random.randint(1, 100)
+
+def build_progression():
+    step = random.randint(1, 10)
+    number_progression = random_generator()
+    progression = []
+    vanished_number = progression
+    while len(progression) < 10:
+        progression.append(number_progression)
+        number_progression += step
+    progression[random.randint(2, len(progression) - 1)] = '. .'
+    return progression, step
+
+
+def main():
+    count = 0
+    while count <= 3:
+        random_number1 = random_generator()
+        random_number2 = random_generator()
+        progression, step = build_progression()
+        answer = prompt.string(f'Question: {progression}\n')
+        print(f'Your answer is: {answer}')
+        if int(answer) == step:
+            count += 1
+            print('Correct!')
+            if count > 2:
+                print(f'Congratulations, {name.capitalize()}!')
+                break
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{step}'.\n"
+                  f"Let's try again, {name.capitalize()}!")
+            break
+
+
+if __name__ == '__main__':
+    main()
