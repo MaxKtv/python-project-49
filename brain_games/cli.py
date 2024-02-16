a346746a97beb7d43a4637b4e7f36c9f9d503585
MaxKtv <159209762+MaxKtv@users.noreply.github.com>
2024-02-16 16:06:59 +1000

    fix hexlet-check error: Update cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,5 +2,5 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string('May I have your Name?\n')
+    name = prompt.string('May I have your Name? ')
     print(f'Hello, {name.capitalize()}!')
