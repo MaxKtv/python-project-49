cbe11c179ddb70235fb0e446e139fda2ad09492a
MaxKtv <maximkutiaev@outlook.com>
2024-02-19 14:17:41 +1000

    fix brain_progression.py

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -11,7 +11,8 @@ def check_task_solution():
     while len(progression) < 10:
         progression.append(number_progression)
         number_progression += step
-    missing_progression = randint(2, len(progression) - 1)
-    progression[missing_progression] = '..'
+    missing_progression = randint(1, len(progression) - 1)
+    progression[missing_progression] = str('..')
     solution = progression[missing_progression - 1] + step
+    progression = ' '.join(str(i) for i in progression)
     return progression, solution
