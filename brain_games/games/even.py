ab5a8dab25512efe316e2d971e8252904d0f8891
MaxKtv <maximkutiaev@outlook.com>
2024-02-21 15:01:14 +1000

    refactor: optimize even.py, prime.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,13 +5,12 @@ TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def is_number_even(number):
-    return number % 2 == 0
+    return number % 2 == 0, 'yes'
 
 
 def generate_qns():
     random_number = randint(1, 100)
-    if is_number_even(random_number):
-        solution = 'yes'
-    else:
+    result, solution = is_number_even(random_number)
+    if not result:
         solution = 'no'
     return random_number, solution
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,6 +1,5 @@
 from random import randint
 
-
 TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
@@ -9,14 +8,13 @@ def is_prime_number(number):
     while number % step != 0:
         step += 1
         if number == 1:
-            return True
-    return step == number
+            return True, 'yes'
+    return step == number, 'yes'
 
 
 def generate_qns():
     random_number = randint(1, 100)
-    if is_prime_number(random_number):
-        solution = 'yes'
-    else:
+    result, solution = is_prime_number(random_number)
+    if not result:
         solution = 'no'
     return random_number, solution
