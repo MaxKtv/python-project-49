dd2eb6bb45864f37eb0222e0c5e2a6d0d1920366
MaxKtv <159209762+MaxKtv@users.noreply.github.com>
2024-02-21 15:42:31 +1000

    fix prime.py
    def is_prime_number(number)

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,14 +5,13 @@ TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime_number(number):
     step = 2
-    while number % step != 0:
-        step += 1
-        if number == 1:
-            return 'yes'
-        elif step == number:
-            return 'yes'
-        else:
+    if number <= 1:
+        return 'no'
+    while step < number:
+        if number % step == 0:
             return 'no'
+        step += 1
+    return 'yes'
 
 
 def generate_qns():
